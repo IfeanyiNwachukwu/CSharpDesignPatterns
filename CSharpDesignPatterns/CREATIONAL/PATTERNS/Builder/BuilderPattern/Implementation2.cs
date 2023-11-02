645023345d0009a8e5f785ab3042657c76a1211a
Ifeanyi Nwachukwu <ifenwachukwu7@outlook.com>
2023-11-02 17:38:19 +0100

    completed the second Implementation for the Builder Design Pattern

---
+++ CSharpDesignPatterns/CREATIONAL/PATTERNS/Builder/BuilderPattern/Implementation2.cs
@@ -0,0 +1,111 @@
+﻿using System.Net.NetworkInformation;
+
+namespace BuilderPattern
+{
+    /// <summary>
+    /// Product
+    /// </summary>
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Dough { get; set; }
+        public string Sauce { get; set; }
+        public string Topping { get; set; }
+
+        public void Display()
+        {
+            Console.WriteLine($"{Name} with {Dough} dough, {Sauce} sauce, and {Topping} topping.");
+        }
+    }
+    /// <summary>
+    /// Builder interface
+    /// </summary>
+    public interface IPizzaBuilder
+    {
+        void Name();
+        void BuildDough();
+        void BuildSauce();
+        void BuildTopping();
+        Pizza GetPizza();
+    }
+    /// <summary>
+    /// Concrete Builder
+    /// </summary>
+    public class MargheritaPizzaBuilder : IPizzaBuilder
+    {
+        private Pizza _pizza;
+        
+        
+        public MargheritaPizzaBuilder()
+        {
+            _pizza = new Pizza();
+        }
+        public void BuildDough()
+        {
+            _pizza.Dough = "Thin crust";
+        }
+
+        public void BuildSauce()
+        {
+            _pizza.Sauce = "Tomato";
+        }
+
+        public void BuildTopping()
+        {
+            _pizza.Topping = "Mozzarella cheese";
+        }
+        public void Name()
+        {
+            _pizza.Name = "Margherita Pizza";
+        }
+        public Pizza GetPizza() => _pizza;
+
+        
+    }
+    /// <summary>
+    /// Concrete Builder
+    /// </summary>
+    public class PepperoniPizzaBuilder : IPizzaBuilder
+    {
+        private Pizza _pizza;
+
+        public PepperoniPizzaBuilder()
+        {
+            _pizza = new Pizza();
+        }
+        public void BuildDough()
+        {
+            _pizza.Dough = "Thick crust";
+        }
+
+        public void BuildSauce()
+        {
+            _pizza.Sauce = "Tomato";
+        }
+
+        public void BuildTopping()
+        {
+            _pizza.Topping = "Pepperoni";
+        }
+        public void Name()
+        {
+            _pizza.Name = "Pepperoni Pizza";
+        }
+        public Pizza GetPizza() => _pizza;
+    }
+    /// <summary>
+    /// Director CLass
+    /// </summary>
+    public class PizzaMaker
+    {
+        public void BuildPizza(IPizzaBuilder pizzaBuilder)
+        {
+            pizzaBuilder.BuildDough();
+            pizzaBuilder.BuildSauce();
+            pizzaBuilder.BuildTopping();
+            pizzaBuilder.Name();
+        }
+    }
+
+
+}
--- CSharpDesignPatterns/CREATIONAL/PATTERNS/Builder/BuilderPattern/Program.cs
+++ CSharpDesignPatterns/CREATIONAL/PATTERNS/Builder/BuilderPattern/Program.cs
@@ -1,1 +1,55 @@
 ﻿
+
+
+using BuilderPattern;
+Console.Title = "Builder";
+
+
+   
+    // Implementation 1
+    var garage = new Garage();
+
+    var miniBuilder = new MiniBuilder();
+    var bmwBuilder = new BMWBuilder();
+
+    garage.Construct(miniBuilder);
+    Console.WriteLine(miniBuilder.Car.ToString());
+    // or: 
+    garage.Show();
+
+    garage.Construct(bmwBuilder);
+    Console.WriteLine(bmwBuilder.Car.ToString());
+    // or: 
+    garage.Show();
+
+  
+
+    // Implementation 2
+    // Create a pizza maker
+    var pizzaMaker = new PizzaMaker();
+
+    // Create a Margherita pizza builder
+    var margheritaBuilder = new MargheritaPizzaBuilder();
+
+    // Build a Margherita pizza
+    pizzaMaker.BuildPizza(margheritaBuilder);
+    var margheritaPizza = margheritaBuilder.GetPizza();
+    margheritaPizza.Display();
+
+    // Create a Pepperoni pizza builder
+    var pepperoniBuilder = new  PepperoniPizzaBuilder ();
+
+    // Build a Pepperoni pizza
+    pizzaMaker.BuildPizza(pepperoniBuilder);
+    var pepperoniPizza = pepperoniBuilder.GetPizza();
+    pepperoniPizza.Display();
+
+
+
+
+
+
+Console.ReadKey();
+
+
+
