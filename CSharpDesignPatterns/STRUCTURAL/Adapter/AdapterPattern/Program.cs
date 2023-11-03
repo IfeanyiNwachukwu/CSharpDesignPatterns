08c8b183ffeb033cc571e17fd95bf74a0bd3b42c
Ifeanyi Nwachukwu <ifenwachukwu7@outlook.com>
2023-11-03 12:56:40 +0100

    Completed Adapter pattern

---
+++ CSharpDesignPatterns/STRUCTURAL/Adapter/AdapterPattern/Program.cs
@@ -0,0 +1,11 @@
+﻿using AdapterPattern;
+
+
+Console.Title = "Adapter";
+
+// object adapter example
+ICityAdapter adapter = new CityAdapter();
+var city = adapter.GetCity();
+
+Console.WriteLine($"{city.FullName}, {city.Inhabitants}");
+Console.ReadKey();
