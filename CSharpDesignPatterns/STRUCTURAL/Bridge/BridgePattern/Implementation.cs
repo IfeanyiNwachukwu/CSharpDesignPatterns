dbbfb227ef2eda086a2703570c718dc5af077655
Ifeanyi Nwachukwu <ifenwachukwu7@outlook.com>
2023-11-04 11:07:28 +0100

    Completed the Bridge Pattern.

---
+++ CSharpDesignPatterns/STRUCTURAL/Bridge/BridgePattern/Implementation.cs
@@ -0,0 +1,66 @@
+﻿namespace BridgePattern
+{
+    /// <summary>
+    /// abstraction
+    /// </summary>
+    public abstract class Menu
+    {
+        public readonly ICoupon _coupon = null!;
+        public abstract int CalculatePrice();
+        public Menu(ICoupon coupon)
+        {
+            _coupon = coupon;
+        }
+    }
+    /// <summary>
+    /// Refined Abstraction
+    /// </summary>
+    public class VegetarianMenu : Menu
+    {
+        public VegetarianMenu(ICoupon coupon):base(coupon)
+        {
+
+        }
+
+        public override int CalculatePrice()
+        {
+            return 20 - _coupon.CouponValue;
+        }
+    }
+
+    /// <summary>
+    /// Refined Abstraction
+    /// </summary>
+    public class MeatBasedMenu : Menu
+    {
+        public MeatBasedMenu(ICoupon coupon) : base(coupon)
+        {
+
+        }
+
+        public override int CalculatePrice()
+        {
+            return 20 - _coupon.CouponValue;
+        }
+    }
+    /// <summary>
+    /// Implementor
+    /// </summary>
+    public interface ICoupon
+    {
+        int CouponValue { get; }
+    }
+    public class NoCoupon : ICoupon
+    {
+        public int CouponValue { get => 0; }
+    }
+
+    public class OneEuroCoupon : ICoupon
+    {
+        public int CouponValue { get => 1; }
+    }
+    public class TwoEuroCoupon : ICoupon
+    {
+        public int CouponValue { get => 2; }
+    }
+}
---
+++ CSharpDesignPatterns/STRUCTURAL/Bridge/BridgePattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using BridgePattern;
+
+Console.Title = "Bridge Design Pattern";
+
+var noCoupon = new NoCoupon();
+var oneEuroCoupon = new OneEuroCoupon();
+
+var meatBasedMenu = new MeatBasedMenu(noCoupon);
+Console.WriteLine($"Meat based menu no coupon : {meatBasedMenu.CalculatePrice()} euro.");
+
+meatBasedMenu = new MeatBasedMenu(oneEuroCoupon);
+Console.WriteLine($"Meat based menu one Euro coupon : {meatBasedMenu.CalculatePrice()} euro.");
+
+var vegetarianMenu = new VegetarianMenu(noCoupon);
+Console.WriteLine($"Vegetarian menu no coupon : {vegetarianMenu.CalculatePrice()} euro.");
+
+vegetarianMenu = new VegetarianMenu(oneEuroCoupon);
+Console.WriteLine($"Vegetarian menu one Euro coupon : {vegetarianMenu.CalculatePrice()} euro.");
+
+
+Console.ReadKey();
+
+
+
